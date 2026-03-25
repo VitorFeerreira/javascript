cdc0149c84d40229e18271fc70428c830665c783
Vitor Ferreira da Silva <vitor1996130@gmail.com>
2026-03-25 10:47:23 -0300

    atualizar aulas

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+console.log("Tudo bem?");
+var a = "Teste";
+var x = 5;
+/*while (x > 0) {
+  console.log(a);
+  x--;
+}
+*/
+/*do {
+  console.log(a);
+  x--;
+} while (x > 0);
+*/
+for (var i = 0; i < 5; i++) {
+  console.log(a);
+}
